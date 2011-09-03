2b4a160be4d7922af2f42905064e3ada5fda0037
Elia Bruni <elia.bruni@gmail.com>
2011-09-03 18:38:50 +0200

    code for lab added

---
+++ vision/compute_lab.m
@@ -0,0 +1,146 @@
+function compute_lab
+
+% Add texton path where to find anigauss
+%path(path, '/home/elia.bruni/google/data_sets/mir/1m/scripts/texton')
+
+conf.calDir = '/Users/eliabruni/data/farhadi/ESP-ImageSet/images' ;
+%conf.calDir = '/Users/eliabruni/data/esp/test/input/esp_sample' ;
+conf.dataDir = '/Users/eliabruni/data/esp/test/ouput/lab' ;
+conf.autoDownloadData = false ;
+conf.numTrain = 9 ;
+conf.numClasses = 1 ;
+conf.numWords = 4 ;
+conf.numSpatialX = 1 ;
+conf.numSpatialY = 1 ;
+conf.quantizer = 'kdtree' ;
+%conf.phowOpts = {'Verbose', 2, 'Step', 5} ;
+
+conf.prefix = 'baseline' ;
+conf.randSeed = 1 ;
+
+
+conf.vocabPath = fullfile(conf.dataDir, [conf.prefix '-vocab.mat']) ;
+conf.modelPath = fullfile(conf.dataDir, [conf.prefix '-model.mat']) ;
+conf.resultPath = fullfile(conf.dataDir, [conf.prefix '-result']) ;
+
+randn('state',conf.randSeed) ;
+rand('state',conf.randSeed) ;
+vl_twister('state',conf.randSeed) ;
+
+% --------------------------------------------------------------------
+%                                                           Setup data
+% --------------------------------------------------------------------
+classes = dir(conf.calDir) ;
+classes = classes([classes.isdir]) ;
+classes = {classes.name} ;
+
+images = {} ;
+imageClass = {} ;
+for ci = 1:length(classes)
+  ims = dir(fullfile(conf.calDir, classes{ci}, '*.jpg'))' ; 
+  ims = cellfun(@(x)fullfile(classes{ci},x),{ims.name},'UniformOutput',false) ;
+  images = {images{:}, ims{:}} ;
+  imageClass{end+1} = ci * ones(1,length(ims)) ;
+end
+imageClass = cat(2, imageClass{:}) ;
+model.classes = classes ;
+
+model.classes = classes ;
+model.numSpatialX = conf.numSpatialX ;
+model.numSpatialY = conf.numSpatialY ;
+model.quantizer = conf.quantizer ;
+model.vocab = [] ;
+
+  
+% --------------------------------------------------------------------
+%                                           Compute spatial histograms
+% --------------------------------------------------------------------
+
+  blockSize = 1000 ;
+  listLength = length(images)/blockSize ;
+  histsNames = [] ;
+  for jj = 1:listLength
+     histsNames{jj} = randseq(30) ;
+  end
+  histsNames = sort(histsNames) ;
+
+
+  hists = {} ;
+  iter = 0 ;
+  for ii = 1:length(images)
+    if mod(ii, 100) == 0
+      fprintf('Processing %s (%.2f %%)\n', images{ii}, 100 * ii / length(images)) ;
+    end
+
+    im = imread(fullfile(conf.calDir, images{ii})) ;
+    hists{ii - (iter * blockSize)} = getColorImage(im, colorNodes);
+
+
+    if mod(ii, blockSize) == 0
+        tmpHists = cat(2, hists{:}) ;
+        tmpHists = rot90(tmpHists) ;
+        histName = histsNames{ii/blockSize} ;
+        eval([histName ' = tmpHists;' ]) ;
+        conf.prefix = histsNames{ii/blockSize} ;
+        conf.histPath = fullfile(conf.dataDir, [conf.prefix '.mat']) ;
+        save(conf.histPath, strcat(histsNames{ii/blockSize})) ;
+        eval([histName ' = {};' ]) ;
+        hists = {} ;
+        tmpHists = {} ;
+        histName = {} ;
+        iter = iter + 1 ;
+
+    end
+
+  end
+  tmpHists = cat(2, hists{:}) ;
+  tmpHists = rot90(tmpHists) ;
+
+  eval(['ZZZZZZZZZZZZZZZZZZZZ' '= tmpHists;' ]) ;
+  % hists = cat(2, hists{:}) ;
+  conf.prefix = 'ZZZZZZZZZZZZZZZZZZZZ' ;
+  conf.histPath = fullfile(conf.dataDir, [conf.prefix '.mat']) ;
+  save(conf.histPath,'ZZZZZZZZZZZZZZZZZZZZ') ;
+
+
+
+
+
+% -------------------------------------------------------------------------
+function im = standarizeImage(im)
+% -------------------------------------------------------------------------
+
+% im = rgb2gray(im) ;
+%im = im2bw(im) ;
+
+if size(im,1) > 480, im = imresize(im, [480 NaN]) ; end
+
+% -------------------------------------------------------------------------
+function counts = getColorImage(im, colorNodes)
+% -------------------------------------------------------------------------
+% colim = getColorImage(im, colorNodes)
+
+[L, a, b] = rgb2lab(im);
+
+%L = (L - mean(L(:)));% / std(L);
+%a = (a - mean(a(:)));% / std(a);
+%b = (b - mean(b(:)));% / std(b);        
+
+feat = single(cat(2, L(:)/2, a(:), b(:))/100); 
+
+%idx = getNearestHierarchy(feat, colorNodes);
+% leafnum = zeros(numel(colorNodes), 1);
+% leafnum([colorNodes.isleaf]) = 1:sum([colorNodes.isleaf]);
+% idx = leafnum(idx);
+
+idx = getNearest(feat, colorNodes.centers);
+
+colorim = reshape(idx, [size(im, 1) size(im, 2)]);
+
+% Count how many times each cluster has been encountered in the image.
+counts = zeros(1,128) ;
+for i = 1:size(colorim,1)
+    counts{colorim(i)} = counts{colorim(i)} + 1 ;
+end
+% Normalization
+counts = single(counts/sum(counts)) ;
