12705b87709273a7c2e6b2add8b673cb8aa4da59
Elia Bruni <elia.bruni@gmail.com>
2011-09-27 10:31:49 +0200

    nearest_neighbors.py refactoring

--- src/sim/util/nearest_neighbors.py
+++ src/sim/util/nearest_neighbors.py
@@ -1,16 +1,3 @@
-'''
-This program is to compute the top-n nearest neighbors from bless list of word-relation.
-Semantic similarity is measured with cosine similalrity.
-Usage:
-
-@param 1: N of top-n nearest neighbors to extract
-@param 2: bless list in format <concept class relation relatum>
-@param 3: features matrix
-@param 4: ouput file
-
------
-'''
-
 import sys, fileinput
 from numpy.linalg import norm
 from numpy import *
@@ -18,21 +5,31 @@ from operator import itemgetter
 from heapq import nlargest
 
 bless_words = []
-bless_types = {}
+bless_types = []
 features = {}
 cosine_pairs = {}
 
-
+words = {}
 
 def read_bless_pairs(pairs_list):
     global bless_pairs 
     pairs_file = open(pairs_list, 'r')
-   
+  
+    last_word = '' 
     for line in pairs_file:
-       	pair = line.split()
-	if pair[0] not in bless_words:
-	    bless_words.append(pair[0])
-	bless_types[pair[3]] = pair[2]
+       	line = line.split()
+        word = line[0]
+        if word == last_word:
+            adj_list = words[word]
+            adj_list[line[3]] = line[2]
+            bless_types.append(line[3])
+	    words[word] = adj_list
+	else:
+	    adj_list = {}
+	    adj_list[line[3]] = line[2]
+	    bless_types.append(line[3])
+	    words[word] = adj_list
+            last_word = word
 
     pairs_file.close()
 
@@ -41,41 +38,43 @@ def read_feature_list(feature_list):
     global bless_words
     global cosine_types
     global features
+    global words
     feature_file = open(feature_list, 'r')
     
     for line in feature_file:
         line = line.split()
 	word = line[0]
-	if word in bless_words or word in bless_types.keys():
+	if word in words or word in bless_types:
     	    features[word] = array(line[1:], dtype=float32)  	
     
     feature_file.close()
 
 def compute_cosines():
+    global words
+    global cosine_pairs
     for word in features:
-        if word in bless_words:
-            for relatum in bless_types.keys:
-		if word in cosine_pairs.keys() and word != relatum and relatum in features:
+        if word in words:
+            for relata in words[word]:
+		if word in cosine_pairs.keys() and relata in features:
 		    word_cosines = cosine_pairs[word]
-		    word_cosines[relatum] = dot(features[word], features[relatum]) / (norm(features[word]) * norm(features[relatum]))
+		    word_cosines[relata] = dot(features[word], features[relata]) / (norm(features[word]) * norm(features[relata]))
 		    cosine_pairs[word] = word_cosines
-		elif word != relatum and relatum in features:
+		elif relata in features:
 		    word_cosines = {}
-	 	    word_cosines[relatum] = dot(features[word], features[relatum]) / (norm(features[word]) * norm(features[relatum]))
-		    cosine_pairs[word] = word_cosines
+		    word_cosines[relata] = dot(features[word], features[relata]) / (norm(features[word]) * norm(features[relata]))
+	 	    cosine_pairs[word] = word_cosines
 
 def get_top(N, output_file):
     global bless_words
     global cosine_pairs
     OFILE = open(output_file, 'w')
     for word in cosine_pairs:
-	if word in bless_words:
-	    word_cosines = cosine_pairs[word]
-	    sorted(word_cosines, key=word_cosines.__getitem__, reverse=True)
-	    topn = nlargest(N, word_cosines.iteritems(), itemgetter(1))
-	    for relatum in topn:
-	        OFILE.write(word + ' ' + bless_types[relatum[0]] + ' ' + relatum[0] + ': ' + str(relatum[1]) + '\n')
-	OFILE.write('\n')
+	word_cosines = cosine_pairs[word]
+	sorted_list = sorted(word_cosines, key=word_cosines.__getitem__, reverse=True)
+	#topn = nlargest(N, word_cosines.iteritems(), itemgetter(1))
+	for relata in sorted_list:
+	    OFILE.write(word + ' ' + words[word][relata] + ' ' + relata + '\n')
+        OFILE.write('\n')
     OFILE.close()		
 			
 	    
@@ -90,8 +89,7 @@ def get_nearest_neighbors(N, pairs_list, feature_list, output_file):
 if __name__=="__main__":
     _pairs_list = sys.argv[1]
     _feature_list = sys.argv[2]
-    _N = eval(sys.argv[3])
-    _output_file = sys.argv[4]
+    _output_file = sys.argv[3]
 
     get_nearest_neighbors(5, _pairs_list, _feature_list, _output_file)
 
